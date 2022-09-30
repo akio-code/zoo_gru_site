67a8fc13675ce69acc5386bf3b682bf0582b2278
Lucas Akio <kikkawa.lucas@outlook.com>
2022-09-29 21:52:08 -0300

    wip: added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,62 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        zoo:{
+            import: './src/index.js',
+            dependOn: 'bootstrap'
+        },
+        bootstrap: './src/vendor/bootstrap.js'
+    },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    // optimization: {
+    //     splitChunks: {
+    //         chunks: 'all'
+    //     }
+    // },
+
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // {
+                    //     loader: 'style-loader'
+                    // },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             title: 'Zoo Gru',
             template: './src/index.html',
